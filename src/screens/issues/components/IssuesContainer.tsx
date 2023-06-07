f0d8a0c61a33a86bd4dee050b7eab582c985b370
Siddharth228 <sasiddharth7@gmail.com>
2023-06-08 03:00:13 +0530

    feat: metatags added and infinite scroller added to window

--- src/screens/issues/components/IssuesContainer.tsx
+++ src/screens/issues/components/IssuesContainer.tsx
@@ -11,40 +11,34 @@ export const IssuesContainer = () => {
   const loadMore = useCallback(() => fetchNextPage(), []);
 
   return (
-    <section
-      id="scrollable-target"
-      className={tx("relative h-[80vh] w-full overflow-auto")}
+    <InfiniteScroller
+      loadMore={loadMore}
+      hasMore={hasNextPage && !isLoading}
+      loader={
+        <div
+          className={tx(
+            "w-full border-t border-t-border-mute flex justify-center px-4 py-6 text-black"
+          )}
+        >
+          Loading...
+        </div>
+      }
     >
-      <InfiniteScroller
-        loadMore={loadMore}
-        hasMore={hasNextPage && !isLoading}
-        useWindow={false}
-        loader={
-          <div
-            className={tx(
-              "w-full border-t border-t-border-mute flex justify-center px-4 py-6 text-black"
-            )}
-          >
-            Loading...
-          </div>
-        }
-      >
-        {data?.pages.length ? (
-          data?.pages.map((issue) =>
-            issue.data.map((issue, index) => (
-              <IssueItem {...issue} index={index} />
-            ))
-          )
-        ) : (
-          <p
-            className={tx(
-              "absolute text-fg-muted top-[50%] left-[50%] text-black -translate-x-[50%] -translate-y-[50%]"
-            )}
-          >
-            No issues found...
-          </p>
-        )}
-      </InfiniteScroller>
-    </section>
+      {data?.pages.length ? (
+        data?.pages.map((issue) =>
+          issue.data.map((issue, index) => (
+            <IssueItem {...issue} index={index} />
+          ))
+        )
+      ) : (
+        <p
+          className={tx(
+            "absolute text-fg-muted top-[50%] left-[50%] text-black -translate-x-[50%] -translate-y-[50%]"
+          )}
+        >
+          No issues found...
+        </p>
+      )}
+    </InfiniteScroller>
   );
 };
